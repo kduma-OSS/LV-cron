ba932f8480c749c881abc1f2920d24857ebbf850
Krystian Duma <git@kduma.tk>
2015-01-30 23:32:55 +0100

    First Version

---
+++ CronServiceProvider.php
@@ -0,0 +1,57 @@
+<?php namespace KDuma\Cron;
+
+use App\Providers\Cron\CronCommand;
+use App\Providers\Cron\CronWorker;
+use Illuminate\Support\ServiceProvider;
+
+class CronServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = true;
+
+	/**
+	 * Bootstrap the application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		//
+	}
+
+	/**
+	 * Register the application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app->singleton('command.queue.cron', function($app)
+		{
+			return new CronCommand($app['queue.worker']);
+		});
+
+		$this->commands('command.queue.cron');
+		$this->app->singleton('queue.worker', function($app)
+		{
+			return new CronWorker($app['queue'], $app['queue.failer'], $app['events']);
+		});
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array(
+			'queue.worker'
+		);
+	}
+
+}
